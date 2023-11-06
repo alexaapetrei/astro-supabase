fa4913d796dbc6bbed1436b70c8e931897394a6e
Alex Apetrei <alex@urssur.com>
2023-11-06 21:45:59 +0100

    new code getting with guard clauses

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -29,7 +29,7 @@ const fetcher: ResourceFetcher<true, GuestbookEntry[], GuestbookEntry> = async (
   return [...data, ...prev];
 };
 
-export function Reviews({ reviews }: { reviews: GuestbookEntry[] }) {
+export function Reviews({ reviews, user }: { reviews: GuestbookEntry[], user: any }) {
   const [data, { refetch }] = createResource(fetcher, {
     initialValue: reviews,
     ssrLoadFrom: "initial",
@@ -50,50 +50,55 @@ export function Reviews({ reviews }: { reviews: GuestbookEntry[] }) {
   return (
     <div class="max-w-3xl w-full">
       <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        <form
-          onsubmit={onSubmitHandler}
-          class="block border bg-blue-100 border-blue-300 rounded-md p-6 dark:bg-blue-950 dark:border-blue-800"
-        >
-          <div>
-            <label
-              class="block mb-1 font-medium dark:text-zinc-300 text-zinc-900 text-sm"
-              for="name"
-            >
-              Name
-            </label>
-            <input
-              id="name"
-              type="text"
-              placeholder="Sam"
-              required
-              name="name"
-              class="w-full block rounded-md py-1 px-3 dark:bg-zinc-800 dark:text-zinc-300 border bg-zinc-50 border-zinc-300 dark:border-zinc-700 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-600 dark:focus:bg-zinc-900 focus:bg-white focus:ring-opacity-60"
-            />
-          </div>
-          <div class="mt-3">
-            <label
-              class="block mb-1 font-medium dark:text-zinc-300 text-zinc-900 text-sm"
-              for="message"
-            >
-              Message
-            </label>
-            <input
-              id="message"
-              type="text"
-              class="w-full block rounded-md py-1 px-3 dark:bg-zinc-800 dark:text-zinc-300 border bg-zinc-50 border-zinc-300 dark:border-zinc-700 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-600 dark:focus:bg-zinc-900 focus:bg-white focus:ring-opacity-60"
-              placeholder="A friendly message"
-              required
-              name="message"
-            />
-          </div>
-          <button
-            class="w-full dark:bg-zinc-100 bg-zinc-900 border-zinc-900 py-1.5 border dark:border-zinc-100 rounded-md mt-4 dark:text-zinc-900 text-zinc-100 font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
-            type="submit"
-            disabled={data.loading}
+        {user ?
+
+          <form
+            onsubmit={onSubmitHandler}
+            class="block border bg-blue-100 border-blue-300 rounded-md p-6 dark:bg-blue-950 dark:border-blue-800"
           >
-            Submit
-          </button>
-        </form>
+            <div>
+              <label
+                class="block mb-1 font-medium dark:text-zinc-300 text-zinc-900 text-sm"
+                for="name"
+              >
+                Name
+              </label>
+              <input
+                id="name"
+                type="text"
+                placeholder="Sam"
+                required
+                name="name"
+                class="w-full block rounded-md py-1 px-3 dark:bg-zinc-800 dark:text-zinc-300 border bg-zinc-50 border-zinc-300 dark:border-zinc-700 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-600 dark:focus:bg-zinc-900 focus:bg-white focus:ring-opacity-60"
+              />
+            </div>
+            <div class="mt-3">
+              <label
+                class="block mb-1 font-medium dark:text-zinc-300 text-zinc-900 text-sm"
+                for="message"
+              >
+                Message
+              </label>
+              <input
+                id="message"
+                type="text"
+                class="w-full block rounded-md py-1 px-3 dark:bg-zinc-800 dark:text-zinc-300 border bg-zinc-50 border-zinc-300 dark:border-zinc-700 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-600 dark:focus:bg-zinc-900 focus:bg-white focus:ring-opacity-60"
+                placeholder="A friendly message"
+                required
+                name="message"
+              />
+            </div>
+            <button
+              class="w-full dark:bg-zinc-100 bg-zinc-900 border-zinc-900 py-1.5 border dark:border-zinc-100 rounded-md mt-4 dark:text-zinc-900 text-zinc-100 font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
+              type="submit"
+              disabled={data.loading}
+            >
+              Submit
+            </button>
+          </form>
+          :
+          <p>Oh shit no user</p>
+        }
         <ul class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
           <For each={data()}>
             {(review) => (
--- src/pages/api/auth/github.ts
+++ src/pages/api/auth/github.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient , type CookieOptions } from '@supabase/ssr'
 import { type APIRoute } from 'astro'
 
 export const GET: APIRoute = async ({ request, cookies, redirect }) => {
@@ -6,45 +6,46 @@ export const GET: APIRoute = async ({ request, cookies, redirect }) => {
   const code = requestUrl.searchParams.get('code')
   const next = requestUrl.searchParams.get('next') || '/'
 
-  if (code) {
-    const supabase = createServerClient(
-      import.meta.env.PUBLIC_SUPABASE_URL,
-      import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
-      {
-        cookies: {
-          get(key) {
-            return cookies.get(key)?.value
-          },
-          set(key, value, options) {
-            cookies.set(key, value, options)
-          },
-          remove(key, options) {
-            cookies.delete(key, options)
-          },
-        },
-      }
-    )
-
-    const { error, data } = await supabase.auth.exchangeCodeForSession(code)
-
-    if (!error) {
-      const { access_token, refresh_token } = data.session;
-      cookies.set("sb-access-token", access_token, {
-        sameSite: "strict",
-        path: "/",
-        secure: true,
-      });
-      cookies.set("sb-refresh-token", refresh_token, {
-        sameSite: "strict",
-        path: "/",
-        secure: true,
-      });
-      // return redirect(`/${next.slice(1)}`)
-      console.log("WAT-- ", next)
-      return redirect(`/dashboard`)
-    }
-  }
+  console.log("WAT --- code dicked", code)
 
   // return the user to an error page with instructions
-  return redirect('/auth/auth-code-error')
+  if (!code) return redirect('/auth/auth-code-error')
+  const supabase = createServerClient(
+    import.meta.env.PUBLIC_SUPABASE_URL,
+    import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(key: string) {
+          return cookies.get(key)?.value;
+        },
+        set(key: string, value: string, options: CookieOptions) {
+          cookies.set(key, value, options);
+        },
+        remove(key: string, options) {
+          cookies.delete(key, options);
+        },
+      },
+    }
+  )
+
+  const { error } = await supabase.auth.exchangeCodeForSession(code)
+
+  if (error) redirect('/auth/auth-code-error')
+  // const { access_token, refresh_token } = data.session;
+  // cookies.set("sb-access-token", access_token, {
+  //   sameSite: "strict",
+  //   path: "/",
+  //   secure: true,
+  // });
+  // cookies.set("sb-refresh-token", refresh_token, {
+  //   sameSite: "strict",
+  //   path: "/",
+  //   secure: true,
+  // });
+  // return redirect(`/${next.slice(1)}`)
+  console.log("WAT-- ", next)
+  return redirect(`/dashboard`)
+
+
+
 }
